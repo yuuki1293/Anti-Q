00fa0bda5a3397c78719a7af7a59e4c19b2edf22
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 07:21:14 +0900

    feat: ExampleMod.java to AntiQ.java

--- src/main/java/com/yuuki1293/antiq/AntiQ.java
+++ src/main/java/com/yuuki1293/antiq/AntiQ.java
@@ -1,28 +1,28 @@
-package com.example.examplemod;
+package com.yuuki1293.antiq;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
+import static com.yuuki1293.antiq.AntiQ.*;
+
+@Mod(modid = MODID, name = NAME, version = VERSION)
+public class AntiQ {
+    public static final String MODID = "antiq";
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
 
-    @EventHandler
+    @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
         logger = event.getModLog();
     }
 
-    @EventHandler
+    @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
         // some example code
