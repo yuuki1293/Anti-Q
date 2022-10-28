5e26c769cd374330a9413a1d9c4c68525cc43a7d
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 07:26:23 +0900

    feat: delete unused

--- src/main/java/com/yuuki1293/antiq/AntiQ.java
+++ src/main/java/com/yuuki1293/antiq/AntiQ.java
@@ -1,10 +1,8 @@
 package com.yuuki1293.antiq;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import org.apache.logging.log4j.Logger;
 
 import static com.yuuki1293.antiq.AntiQ.*;
 
@@ -14,18 +12,13 @@ public class AntiQ {
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
-
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-        logger = event.getModLog();
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
