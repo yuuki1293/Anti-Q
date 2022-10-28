69239475df0455dfeed0ba6f8b56c724278ed14a
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 07:50:34 +0900

    feat: register ClientEvent

--- src/main/java/com/yuuki1293/antiq/AntiQ.java
+++ src/main/java/com/yuuki1293/antiq/AntiQ.java
@@ -1,7 +1,11 @@
 package com.yuuki1293.antiq;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLConstructionEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 import static com.yuuki1293.antiq.AntiQ.*;
@@ -12,13 +16,31 @@ public class AntiQ {
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
 
+    public static final String CLIENT_PROXY = "com.yuuki1293." + MODID + ".proxy.ClientProxy";
+
+    @SidedProxy(clientSide = CLIENT_PROXY)
+    public static ClientProxy proxy;
+
+    @Mod.EventHandler
+    public void construct(FMLConstructionEvent event) {
+        MinecraftForge.EVENT_BUS.register(proxy);
+    }
+
+
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
+        proxy.preInit(event);
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
+        proxy.init(event);
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
     }
 }
--- src/main/java/com/yuuki1293/antiq/ClientProxy.java
+++ src/main/java/com/yuuki1293/antiq/ClientProxy.java
@@ -1,5 +1,6 @@
 package com.yuuki1293.antiq;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -8,6 +9,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod.EventBusSubscriber(modid = AntiQ.MODID)
 public class ClientProxy {
     public void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(new ClientEvent());
     }
 
     public void init(FMLInitializationEvent event) {
