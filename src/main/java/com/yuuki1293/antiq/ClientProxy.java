2d375e29dda0f13af00990b00e226770cebf003e
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 07:29:13 +0900

    feat: add ClientProxy

---
+++ src/main/java/com/yuuki1293/antiq/ClientProxy.java
@@ -0,0 +1,18 @@
+package com.yuuki1293.antiq;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod.EventBusSubscriber(modid = AntiQ.MODID)
+public class ClientProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    public void init(FMLInitializationEvent event) {
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
