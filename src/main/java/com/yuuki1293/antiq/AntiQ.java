0e8bc6a27bfc8db1b23a4c751e764140f77eb5ea
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 22:43:40 +0900

    chore: update NAME VERSION

--- src/main/java/com/yuuki1293/antiq/AntiQ.java
+++ src/main/java/com/yuuki1293/antiq/AntiQ.java
@@ -15,8 +15,8 @@ import static com.yuuki1293.antiq.AntiQ.*;
 @Mod(modid = MODID, name = NAME, version = VERSION)
 public class AntiQ {
     public static final String MODID = "antiq";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String NAME = "Anti Q";
+    public static final String VERSION = "1.0.0";
 
     public static final String CLIENT_PROXY = "com.yuuki1293." + MODID + ".proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.yuuki1293." + MODID + ".proxy.ClientProxy";
