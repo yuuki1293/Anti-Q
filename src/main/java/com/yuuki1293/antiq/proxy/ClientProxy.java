88799a41ebaeb6ee4c92902df348fc99b6a17677
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 15:09:28 +0900

    feat: init TossKeyManager

--- src/main/java/com/yuuki1293/antiq/proxy/ClientProxy.java
+++ src/main/java/com/yuuki1293/antiq/proxy/ClientProxy.java
@@ -1,33 +1,15 @@
 package com.yuuki1293.antiq.proxy;
 
-import com.yuuki1293.antiq.ClientEvent;
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.settings.IKeyConflictContext;
+import com.yuuki1293.antiq.TossKeyManager;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
-    boolean active_q = false;
-
     public void preInit(FMLPreInitializationEvent event) {
-        Minecraft
-                .getMinecraft()
-                .gameSettings
-                .keyBindDrop
-                .setKeyConflictContext(new IKeyConflictContext() {
-                    @Override
-                    public boolean isActive() {
-                        return active_q;
-                    }
-
-                    @Override
-                    public boolean conflicts(IKeyConflictContext other) {
-                        return false;
-                    }
-                });
-        MinecraftForge.EVENT_BUS.register(new ClientEvent());
+        TossKeyManager.init();
+        MinecraftForge.EVENT_BUS.register(new TossKeyManager());
     }
 
     public void init(FMLInitializationEvent event) {
