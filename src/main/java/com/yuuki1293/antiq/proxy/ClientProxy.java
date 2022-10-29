bfb319e422c21aed934f0e96c9bb5066108872d2
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 09:28:10 +0900

    feat: ClientProxy extends CommonProxy

--- src/main/java/com/yuuki1293/antiq/proxy/ClientProxy.java
+++ src/main/java/com/yuuki1293/antiq/proxy/ClientProxy.java
@@ -1,13 +1,14 @@
-package com.yuuki1293.antiq;
+package com.yuuki1293.antiq.proxy;
 
+import com.yuuki1293.antiq.AntiQ;
+import com.yuuki1293.antiq.ClientEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod.EventBusSubscriber(modid = AntiQ.MODID)
-public class ClientProxy {
+public class ClientProxy extends CommonProxy {
     public void preInit(FMLPreInitializationEvent event) {
         MinecraftForge.EVENT_BUS.register(new ClientEvent());
     }
