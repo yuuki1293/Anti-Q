dfc1c467b3d3fe2317539441e0d80d2aa961f60a
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 14:47:19 +0900

    feat: TossKeyManager
    init
    click

--- src/main/java/com/yuuki1293/antiq/TossKeyManager.java
+++ src/main/java/com/yuuki1293/antiq/TossKeyManager.java
@@ -1,4 +1,32 @@
 package com.yuuki1293.antiq;
 
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.settings.IKeyConflictContext;
+
 public class TossKeyManager {
+    private static int q_count = 0;
+
+    public static void init() {
+        Minecraft
+                .getMinecraft()
+                .gameSettings
+                .keyBindDrop
+                .setKeyConflictContext(new IKeyConflictContext() {
+                    @Override
+                    public boolean isActive() {
+                        return q_count >= 1;
+                    }
+
+                    @Override
+                    public boolean conflicts(IKeyConflictContext other) {
+                        return false;
+                    }
+                });
+    }
+
+    public static void click() {
+        q_count++;
+        if (q_count > 1)
+            q_count = 0;
+    }
 }
