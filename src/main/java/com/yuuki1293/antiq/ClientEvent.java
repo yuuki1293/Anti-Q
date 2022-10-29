9a83ae4fb4d6e0fb6a8e57ee1d3292eddd05e142
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 14:43:21 +0900

    fix: error

--- src/main/java/com/yuuki1293/antiq/ClientEvent.java
+++ src/main/java/com/yuuki1293/antiq/ClientEvent.java
@@ -1,11 +1,8 @@
 package com.yuuki1293.antiq;
 
-import net.minecraft.client.Minecraft;
-import net.minecraftforge.client.settings.IKeyConflictContext;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
 public class ClientEvent {
     public void onTossItem(InputEvent.KeyInputEvent event) {
-        event.
     }
 }
