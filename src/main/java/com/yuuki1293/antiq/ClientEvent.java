88613aa18eb842bbad198ac3da7d8024fe7c9907
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 15:07:10 +0900

    feat: delete ClientEvent.java

--- src/main/java/com/yuuki1293/antiq/ClientEvent.java
+++
@@ -1,8 +0,0 @@
-package com.yuuki1293.antiq;
-
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-
-public class ClientEvent {
-    public void onTossItem(InputEvent.KeyInputEvent event) {
-    }
-}
