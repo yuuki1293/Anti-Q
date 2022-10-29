4a3ef303475c465982ddf8bfe055cc3c817fd237
yuuki1293 <yuuki1293@outlook.jp>
2022-10-29 09:27:20 +0900

    feat: add CommonProxy

---
+++ src/main/java/com/yuuki1293/antiq/proxy/CommonProxy.java
@@ -0,0 +1,19 @@
+package com.yuuki1293.antiq.proxy;
+
+import com.yuuki1293.antiq.AntiQ;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod.EventBusSubscriber(modid = AntiQ.MODID)
+abstract public class CommonProxy {
+    public void preInit(FMLPreInitializationEvent event) {
+    }
+
+    public void init(FMLInitializationEvent event) {
+    }
+
+    public void postInit(FMLPostInitializationEvent event) {
+    }
+}
